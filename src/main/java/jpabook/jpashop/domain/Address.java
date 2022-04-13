98694f6e1a8e97417a7656f7304a9de33e224b7f
hyerin-jang <hrjang92@gmail.com>
2022-04-13 20:29:45 +0900

    value type mapping

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+
+    @Column(length = 10)
+    private String city;
+
+    @Column(length = 20)
+    private String street;
+
+    @Column(length = 5)
+    private String zipcode;
+
+    public String fullAddress () {
+        return getCity() + getStreet() + getZipcode();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    // using getters during code generation 사용하는 것이 좋음
+    // 사용하지 않으면 필드에 직접 접근하는데 proxy일 경우 접근 불가능 하기 때문
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
+}
